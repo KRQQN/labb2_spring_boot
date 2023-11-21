1929cadc1d27a87d5bade2b4e0bc2c1c27cf1cac
Rkron91 <robin.kron@lerniastudent.se>
2023-11-21 16:00:18 +0100

    initial commit

---
+++ src/test/java/com/example/labb2_spring_boot/Labb2SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labb2_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labb2SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
